c7a257680eefe0f6c16b46a9a9452ebc178a5126
caleb olusegun <olupeniel2000@gmail.com>
2022-05-25 22:51:34 +0100

    learning loops

--- buildfacebook.js
+++ buildfacebook.js
@@ -1,31 +1,43 @@
-var database = [
-    {
-        username: "andrei",
-        password: "supersecret"
-    }
-];
-var newsFeed = [
-    {
-        username:"bobby",
-        timeline:"so tired from all that learning!"
-    },
+// var database = [
+//     {
+//         username: "andrei",
+//         password: "supersecret"
+//     }
+// ];
+// var newsFeed = [
+//     {
+//         username:"bobby",
+//         timeline:"so tired from all that learning!"
+//     },
 
 
-    {
-        username: "sally",
-        timeline: "javascript is so cool"
-    }
+//     {
+//         username: "sally",
+//         timeline: "javascript is so cool"
+//     }
+// ]
+// var userNamePrompt = prompt("What is your username");
+// var passwordPrompt = prompt("What is your password");
+
+// function signIn(user,pass) {
+//     if(user === database[0].username && pass === database[0].password ){
+//         console.log( newsFeed);
+//     }else{
+//         alert("Sorry Not the USER ");
+//     }
+    
+// }
+// signIn(userNamePrompt,passwordPrompt);
+//looping
+
+var toDo = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy",
 ]
-var userNamePrompt = prompt("What is your username");
-var passwordPrompt = prompt("What is your password");
-
-function signIn(user,pass) {
-    if(user === database[0].username && pass === database[0].password ){
-        console.log( newsFeed);
-    }else{
-        alert
-        ("Sorry Not the USER ");
-    }
+for (let i = 0; i < toDo.length; i++) {
+    console.log(i);
     
 }
-signIn(userNamePrompt,passwordPrompt);
